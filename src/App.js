0392c3e810e19fea83ec7e081547cab1560b5c1e
shintaroDesu <snkraishin87@gmail.com>
2022-11-17 11:01:45 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import React, { useState } from 'react';
 import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 import ImagePreview from './ImagePreview';
+import Compress from "react-image-file-resizer";
+
+
+function dataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  var byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  var ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
+
+}
 
 
 
@@ -14,7 +41,7 @@ function App (props) {
         data = JSON.stringify({image: base64});
     httpPost.onreadystatechange = function(err) {
       console.log("sent")
-      if (httpPost.readyState == 4 && httpPost.status == 200) {
+      if (httpPost.readyState == 4 && (httpPost.status == 200 || httpPost.status == 301)) {
         setDataUri(httpPost.response);
       } else {
         console.log(err);
@@ -23,15 +50,29 @@ function App (props) {
     console.log("enterd");
     // httpPost.setHeader('Content-Type', 'application/json');
     httpPost.open("POST", path, true);
+    //httpPost.setestHeader("Access-Control-Allow-Origin", "https://138.195.44.21:8000");
+    //httpPost.setRequestHeader("ngrok-skip-browser-warning", "true");
     console.log('zzzz');
     httpPost.send(data);
   };
 
   async function handleTakePhoto (dataUri) {
     console.log("hello")
-    sendBase64ToServer(dataUri)
-
+    Compress.imageFileResizer(
+        dataURItoBlob(dataUri), // the file from input
+        480, // width
+        480, // height
+        "JPEG", // compress format WEBP, JPEG, PNG
+        70, // quality
+        0, // rotation
+        (uri) => {
+          sendBase64ToServer(uri)
+          // You upload logic goes here
+        },
+        "base64" // blob or base64 default base64
+    );
   }
+
 
   const isFullscreen = false;
   return (
